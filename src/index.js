d2f5ef441f3e932be532e853ebcdefc89de534a9
Gregory Kysenko <heyapple1395@gmail.com>
2018-07-06 13:43:33 +0300

    url fixed

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
         });
             try {
                 const phone = await chromeless
-                    .goto('https://www.olx.ua/obyavlenie/televizor-samsung-24-dyuyma-samsung-15-17-9-22-32-40-full-hd-t2-IDkL3k.html#cc5c316dc0;promoted')
+                    .goto(req._parsedUrl.query)
                     .wait(50)
                     .click('li > div[data-rel="phone"]')
                     .wait(300)
