8445f0d8cf29b93f686a4deba2291c728cac905e
Gregory Kysenko <heyapple1395@gmail.com>
2018-07-23 17:54:35 +0300

    hello

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
 'use strict';
-const Chromeless  = require('chromeless').default;
+const nightmare = require('nightmare')();
 module.exports = function (app) {
-    app.get(`/olxPhone`, async (req, res) => {
-        const chromeless = new Chromeless({
-                remote: {
-                    endpointUrl: 'https://8xziiescfe.execute-api.us-east-1.amazonaws.com/dev/',
-                    apiKey: 'h1xms7s5P35qLMfhHNxPO11CpQyxVOaSa3OttC6t',
-                },
-                scrollBeforeClick: true,
-                implicitWait: true
-        });
-            try {
-                const phone = await chromeless
-                    .goto('https://www.olx.ua/obyavlenie/televizor-samsung-24-dyuyma-samsung-15-17-9-22-32-40-full-hd-t2-IDkL3k.html#cc5c316dc0;promoted')
-                    .wait(50)
-                    .click('li > div[data-rel="phone"]')
-                    .wait(300)
-                    .evaluate(() => {
-                        return document.querySelector('div[data-rel="phone"] > strong.xx-large').textContent;
-                    });
-        
-                res.send(phone);
-            } catch (err) {
-                res.send(err)
-            }
-                   
-            await chromeless.end();
-                                     
+    app.get(`/olxPhone`, (req, res) => {
+
+        nightmare
+            .goto(req._parsedUrl.query)
+            .wait(100)
+            .click('li > div[data-rel="phone"]')
+            .wait(280)
+            .evaluate(() => {
+                return document.querySelector('div[data-rel="phone"] > strong.xx-large').textContent;
+            })
+            .end()
+            .then((data) => {
+                console.log('1');
+                res.send(JSON.stringify({
+                    phone: data
+                }));
+            })   
+            .catch((err) => {
+                console.log('0');
+                
+                res.send(err);
+            });          
+                                                 
     });
 };
